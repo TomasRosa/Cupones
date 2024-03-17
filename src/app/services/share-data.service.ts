36a4eea95c063b94a8a115e9d4f7319be7bf2354
TomasRosa <tomilautialdosivi@gmail.com>
2024-03-17 12:00:42 -0300

    lo subo

---
+++ src/app/services/share-data.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../enviroments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Lugar } from '../interfaces/lugar';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShareDataService {
+  
+  URL = environment.urlApi;
+  constructor(private http: HttpClient) {}
+  
+//   obtenerDatosSegunId(idCategoria: number): any[] {
+//     return this.lugares.filter((lugar) => lugar.idCategoria === idCategoria);
+//   }
+//   filtrarPorLugares(consulta: string): any {
+//     return this.lugares.filter(lugar =>
+//         lugar.nombre.toLowerCase().includes(consulta.toLowerCase()) || // Buscar por nombre del lugar
+//         lugar.nombreCategoria.toLowerCase().includes(consulta.toLowerCase()) // Buscar por nombre de categoría
+//     );
+// }
+ getLugares(): Observable<Lugar[]>
+ {
+    return this.http.get<Lugar[]>(`${this.URL}/lugares`);
+ }
+
+}
