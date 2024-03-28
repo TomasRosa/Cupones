a0017c542ed79a28c81c5a6648fd0e5ca6b7368d
TomasRosa <tomilautialdosivi@gmail.com>
2024-03-28 16:00:35 -0300

    toggle menu

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,84 +1,97 @@
-import { Component, OnInit } from '@angular/core';
-import { NavigateToService } from '../../services/navigate-to.service';
-import { PopupService } from '../../services/pop-up-service.service';
+import { Component, HostListener, OnInit } from "@angular/core";
+import { NavigateToService } from "../../services/navigate-to.service";
+import { PopupService } from "../../services/pop-up-service.service";
 import { PopupComponent } from "../popup/popup.component";
-import { Lugar } from '../../interfaces/lugar';
-import { ShareDataService } from '../../services/share-data.service';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { VerDetallesService } from '../../services/ver-detalles.service';
-import { AuthService } from '../../services/auth.service';
-import { Observable } from 'rxjs';
+import { Lugar } from "../../interfaces/lugar";
+import { ShareDataService } from "../../services/share-data.service";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { VerDetallesService } from "../../services/ver-detalles.service";
+import { AuthService } from "../../services/auth.service";
+import { Observable } from "rxjs";
 
 @Component({
-    selector: 'app-navbar',
-    standalone: true,
-    templateUrl: './navbar.component.html',
-    styleUrls: ['./navbar.component.css'],
-    imports: [PopupComponent,CommonModule,FormsModule]
+  selector: "app-navbar",
+  standalone: true,
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.css"],
+  imports: [PopupComponent, CommonModule, FormsModule],
 })
-export class NavbarComponent implements OnInit{
-  constructor(private navigateTo: NavigateToService, 
-    private popUpService: PopupService, 
+export class NavbarComponent implements OnInit {
+  constructor(
+    private navigateTo: NavigateToService,
+    private popUpService: PopupService,
     private shareData: ShareDataService,
     private verDetalle: VerDetallesService,
-    private auth: AuthService) {}
-  
-  terminoBusqueda: string = '';
+    private auth: AuthService
+  ) {}
+
+  terminoBusqueda: string = "";
   resultadosBusqueda: Lugar[] = [];
-  mensajeLogout: string = '';
+  mensajeLogout: string = "";
+
   isUserLoggedIn$!: Observable<boolean>;
-    
+
+  isMenuOpen: boolean = false;
+
+  nombreUsuario$!: Observable<string | null>;
+  emailUsuario$!: Observable<string | null>;
+  apellidoUsuario$!: Observable<string | null>;
+
   ngOnInit(): void {
     this.isUserLoggedIn$ = this.auth.isLoggedIn();
-    this.isUserLoggedIn$.subscribe(isLoggedIn => {
+    this.isUserLoggedIn$.subscribe((isLoggedIn) => {
       if (isLoggedIn) {
-        console.log('El usuario está logueado');
+        console.log("El usuario está logueado");
+        this.nombreUsuario$ = this.auth.getName();
+        console.log(this.nombreUsuario$);
+        this.emailUsuario$ = this.auth.getEmail();
+        console.log(this.emailUsuario$);
+        this.apellidoUsuario$ = this.auth.getLastName();
       } else {
-        console.log('El usuario no está logueado');
+        console.log("El usuario no está logueado");
       }
     });
   }
-
-  openPopup()
-  {
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+  openPopup() {
     this.popUpService.open();
   }
-
   filtrarLugares() {
-    if (this.terminoBusqueda.trim() !== '') {
-      this.shareData.filtrarPorLugares(this.terminoBusqueda).subscribe((data) => {
-        this.resultadosBusqueda = data;
-      });
+    if (this.terminoBusqueda.trim() !== "") {
+      this.shareData
+        .filtrarPorLugares(this.terminoBusqueda)
+        .subscribe((data) => {
+          this.resultadosBusqueda = data;
+        });
     } else {
       this.resultadosBusqueda = [];
     }
   }
   verDetalles(id: number) {
-    const detalles = this.resultadosBusqueda.find(lugar => lugar.id === id);
+    const detalles = this.resultadosBusqueda.find((lugar) => lugar.id === id);
     if (detalles) {
-        this.verDetalle.setDetallesProducto(detalles); // Enviamos los detalles al servicio
-        this.navigateToDetalles(id);
+      this.verDetalle.setDetallesProducto(detalles); // Enviamos los detalles al servicio
+      this.navigateToDetalles(id);
     }
-}
+  }
   navigateToDetalles(id: number) {
     // Aquí navegas al componente de detalles con los detalles del producto
-    this.navigateTo.navigateTo('/detalles/' + id);
+    this.navigateTo.navigateTo("/detalles/" + id);
     this.resultadosBusqueda = [];
-    this.terminoBusqueda = '';
+    this.terminoBusqueda = "";
   }
-  navigateTos(route: string)
-  {
+  navigateTos(route: string) {
     this.navigateTo.navigateTo(route);
   }
-  logout()
-  {
-    this.auth.logout()
-    .then(response => {
+  logout() {
+    this.auth.logout().then((response) => {
       console.log(response);
-      this.mensajeLogout = '¡Hasta luego!';
-      this.navigateTo.navigateTo('/inicio');
-    })
+      this.mensajeLogout = "¡Hasta luego!";
+      this.navigateTo.navigateTo("/inicio");
+    });
   }
   isLoggedIn(): Observable<boolean> {
     return this.auth.isLoggedIn();
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,23 +1,35 @@
 import { Injectable } from '@angular/core';
 import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, signInWithPopup,GoogleAuthProvider} from '@angular/fire/auth';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private userSubject = new BehaviorSubject<boolean>(false);
+  private userDataSubject = new BehaviorSubject<any>(null);
 
   constructor(private auth: Auth) {
     this.auth.onAuthStateChanged(user => {
-      if (!!user) {
+      if (user) {
         this.setUserLoggedIn(false); // Emitir false si el usuario no está autenticado
       } else {
         this.setUserLoggedIn(true); // Emitir true si el usuario está autenticado
       }
     });
   }
+  getName(): Observable<string | null> {
+    return this.userDataSubject.pipe(map(userData => userData ? userData.name : null));
+  }
 
+  getLastName(): Observable<string | null> {
+    return this.userDataSubject.pipe(map(userData => userData ? userData.lastName : null));
+  }
+
+  getEmail(): Observable<string | null> {
+    return this.userDataSubject.pipe(map(userData => userData ? userData.email : null));
+  }
   register(email: any, password: any)
   {
     return createUserWithEmailAndPassword(this.auth,email,password);
